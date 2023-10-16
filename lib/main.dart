37b756a413321a173c8fa464985b7bd03df73914
Aline Git <aline.barreto98@hotmail.com>
2023-10-16 17:26:31 -0300

    Adição de novo modelo da Calculador IMC

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,130 @@
-import 'dart:io';
-import 'class/pessoa.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  calculadoraIMC();
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
 }
 
-calculadoraIMC() {
-  try {
-    stdout.write("==== Digite seu nome ====\n");
-    String nome = stdin.readLineSync()!;
+class Home extends StatefulWidget {
+  @override
+  Homestate createState() => Homestate();
+}
 
-    stdout.write("==== Digite seu peso ====\n");
-    double peso = double.parse(stdin.readLineSync()!);
+class Homestate extends State<Home> {
+  String _info = "Informe seus dados.";
 
-    stdout.write("==== Digite sua altura ====\n");
-    double altura = double.parse(stdin.readLineSync()!);
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-    if (peso <= 0 || altura <= 0) {
-      print("Peso e altura devem ser valores positivos e maiores que zero.");
-      return;
-    }
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
 
-    Pessoa pessoa = Pessoa(nome, peso, altura);
-    print("\nNome: ${pessoa.nome}");
-    double resultado = pessoa.calcImcExpression().roundToDouble();
-    print("O valor do IMC é: ${resultado.toStringAsFixed(2)}");
+  void _resetFields() {
+    pesoController.text = '';
+    alturaController.text = '';
+    setState(() {
+      _info = "Informe seus dados.";
+      _formKey = GlobalKey<FormState>();
+    });
+  }
 
-    String statusCorporal = pessoa.getStatusCorporal(resultado);
-    print("Seu status corporal: $statusCorporal\n");
-  } catch (e) {
-    print("Ocorreu um erro: $e");
+  void _calcular() {
+    setState(() {
+      double peso = double.parse(pesoController.text);
+      double altura = double.parse(alturaController.text) / 100;
+      double imc = peso / (altura * altura);
+      print(imc);
+      if (imc < 18.6) {
+        _info = 'Abaixo do Peso (${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _info = 'Peso Ideal (${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _info = 'Levemente Acima do Peso (${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _info = 'Obesidade Grau I (${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _info = 'Obesidade Grau II (${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 40) {
+        _info = 'Obesidade Grau III (${imc.toStringAsPrecision(3)})';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Calcula IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.refresh), onPressed: _resetFields)
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                        labelText: "Peso (kg)",
+                        labelStyle: TextStyle(color: Colors.green)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25.0),
+                    controller: pesoController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Insira seu Peso!";
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Altura (CM)",
+                        labelStyle: TextStyle(color: Colors.green)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25.0),
+                    controller: alturaController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Insira sua Altura!";
+                      }
+                      return null;
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    child: Container(
+                      height: 50,
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            _calcular();
+                          }
+                        },
+                        child: Text(
+                          'Calcular',
+                          style: TextStyle(color: Colors.white, fontSize: 25.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    _info,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25),
+                  )
+                ],
+              ),
+            )));
   }
 }
